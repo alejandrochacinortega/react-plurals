18d59ba7ed922d09300951dd45018db013dfae27
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-11 15:07:50 +0200

    create cards first commit

--- script.jsx
+++ script.jsx
@@ -1,43 +1,38 @@
-var Button = React.createClass({
-  localHandler: function () {
-    this.props.localHandler(this.props.increment)
-  },
-  render         : function () {
-    return (
-      <button onClick={this.localHandler}>+{this.props.increment}</button>
-    )
-  }
-});
-
-var Result = React.createClass({
-  render: function () {
-    return (
-      <div>{this.props.localCounter}</div>
-    )
-  }
-});
-
-var Main = React.createClass({
+var Card = React.createClass({
   getInitialState: function () {
-    return {
-      counter: 10
-    }
+    return {}
   },
-  handleClick    : function (increment) {
-    console.log('you click me ');
-    this.setState({counter: this.state.counter + increment})
-  },
-  render: function () {
+
+  componentDidMount: function () {
+    var component = this;
+    $.get("https://api.github.com/users/" + this.props.login, function (data) {
+      console.log('data  ', data);
+      component.setState(data);
+    })
+  }
+  ,
+  render           : function () {
     return (
       <div>
-        <Button localHandler={this.handleClick} increment={1}/>
-        <Button localHandler={this.handleClick} increment={5}/>
-        <Button localHandler={this.handleClick} increment={10}/>
-        <Button localHandler={this.handleClick} increment={100}/>
-        <Result localCounter={this.state.counter}/>
+        <img src={this.state.avatar_url} width="80"/>
+        <h3>{this.state.login}</h3>
+        <hr/>
       </div>
     )
   }
 });
 
-React.render(<Main />, document.getElementById("root"));
+
+var Main = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Card login="peterhunt"/>
+        <Card login="johnpapa"/>
+      </div>
+    )
+  }
+});
+
+React.render(
+  <Main />, document.getElementById("root"));
