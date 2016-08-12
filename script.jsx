1f1c05a736f87306b16b4723093dbf81b401cc49
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-12 14:34:25 +0200

    Play nince first commit

--- script.jsx
+++ script.jsx
@@ -1,65 +1,90 @@
-var Card = React.createClass({
-  getInitialState: function () {
-    return {}
-  },
+/*------------- CARD COMPONENTS ---------------*/
 
-  componentDidMount: function () {
-    var component = this;
-    $.get("https://api.github.com/users/" + this.props.login, function (data) {
-      console.log('data  ', data);
-      component.setState(data);
-    })
-  }
-  ,
-  render           : function () {
-    return (
-      <div>
-        <img src={this.state.avatar_url} width="80"/>
-        <h3>{this.state.login}</h3>
-        <hr/>
-      </div>
-    )
-  }
-});
+// var Card = React.createClass({
+//   getInitialState: function () {
+//     return {}
+//   },
+//
+//   componentDidMount: function () {
+//     var component = this;
+//     $.get("https://api.github.com/users/" + this.props.login, function (data) {
+//       console.log('data  ', data);
+//       component.setState(data);
+//     })
+//   }
+//   ,
+//   render           : function () {
+//     return (
+//       <div>
+//         <img src={this.state.avatar_url} width="80"/>
+//         <h3>{this.state.login}</h3>
+//         <hr/>
+//       </div>
+//     )
+//   }
+// });
+//
+// var Form = React.createClass({
+//   handleSubmit: function(e) {
+//     console.log('submitting now ');
+//     e.preventDefault();
+//     var loginInput = React.findDOMNode(this.refs.login);
+//     this.props.addCard(loginInput.value);
+//     loginInput.value = ";"
+//   },
+//   render: function () {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <input placeholder="github login" ref="login"/>
+//         <button>Add</button>
+//       </form>
+//     )
+//   }
+// });
+//
+//
+// var Main = React.createClass({
+//   getInitialState: function () {
+//     return {logins: []}
+//   },
+//   addCard: function (loginToAdd) {
+//     this.setState({logins: this.state.logins.concat(loginToAdd)})
+//   },
+//   render         : function () {
+//     var cards = this.state.logins.map(function (login) {
+//       return (<Card login={login}/>)
+//     });
+//     return (
+//       <div>
+//         <Form addCard={this.addCard}/>
+//         {cards}
+//       </div>
+//     )
+//   }
+// });
+//
+// React.render(
+//   <Main />, document.getElementById("root"));
 
-var Form = React.createClass({
-  handleSubmit: function(e) {
-    console.log('submitting now ');
-    e.preventDefault();
-    var loginInput = React.findDOMNode(this.refs.login);
-    this.props.addCard(loginInput.value);
-    loginInput.value = ";"
-  },
-  render: function () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input placeholder="github login" ref="login"/>
-        <button>Add</button>
-      </form>
-    )
-  }
-});
+/*------------- CARD COMPONENTS ---------------*/
 
+
+/*------------- KID GAME: PLAY NINE ---------------*/
 
 var Main = React.createClass({
-  getInitialState: function () {
-    return {logins: []}
-  },
-  addCard: function (loginToAdd) {
-    this.setState({logins: this.state.logins.concat(loginToAdd)})
-  },
-  render         : function () {
-    var cards = this.state.logins.map(function (login) {
-      return (<Card login={login}/>)
-    });
+  render: function () {
     return (
-      <div>
-        <Form addCard={this.addCard}/>
-        {cards}
-      </div>
+      <h2>react</h2>
     )
   }
 });
 
 React.render(
-  <Main />, document.getElementById("root"));
+  <Main />, document.getElementById("container")
+);
+
+
+
+
+
+/*------------- KID GAME: PLAY NINE ---------------*/
